47368af7662fd17817cad215b928ea0303fb1ced
Nam <nam.mirbeau@gmail.com>
2020-02-29 13:08:25 +0100

    Commit du projet

---
+++ Assets/Scripts/EventDetection.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventDetection : MonoBehaviour
+{
+    [SerializeField]
+    bool isEventVision = false;
+    [SerializeField]
+    bool canRepeatTrigger = false;
+
+    [SerializeField]
+    UnityEvent unityEvent;
+
+    Transform player;
+
+    IEnumerator coroutineSight = null;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if(isEventVision == false)
+        {
+            if (other.tag == "Player")
+            {
+                unityEvent.Invoke();
+                DesactivateEvent();
+            }
+        }
+        else
+        {
+            if (other.tag == "Vision")
+            {
+                player = other.transform.root;
+                if (coroutineSight != null)
+                    StopCoroutine(coroutineSight);
+                coroutineSight = CheckPlayerInSight();
+                StartCoroutine(coroutineSight);
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Vision")
+        {
+            if (coroutineSight != null)
+                StopCoroutine(coroutineSight);
+        }
+    }
+
+    private IEnumerator CheckPlayerInSight()
+    {
+        while (true)
+        {
+            // On check le Layer 0 voir si on touche le joueur, si on le touche, c'est que c'est bon.
+            int layerMask = 1 << 0;
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, 20, layerMask))
+            {
+                Debug.Log(hit.collider.name);
+                if (hit.collider.tag == "Player")
+                {
+                    unityEvent.Invoke();
+                    StopCoroutine(coroutineSight);
+                    DesactivateEvent();
+                }
+            }
+            yield return null;
+        }
+    }
+
+    private void DesactivateEvent()
+    {
+        if (canRepeatTrigger == true)
+            return;
+        this.gameObject.SetActive(false);
+    }
+
+
+}
---
+++ Assets/_project/Scripts/PlayerCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    Camera cameraComponent;
+
+    IEnumerator fovCoroutine;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void ChangeFOV(float targetFOV)
+    {
+        ChangeFOV(targetFOV, 60);
+    }
+
+    public void ChangeFOV(float targetFOV, int time = 60)
+    {
+        if (fovCoroutine != null)
+            StopCoroutine(fovCoroutine);
+        fovCoroutine = FieldOfViewTransition(targetFOV, time);
+        StartCoroutine(fovCoroutine);
+    }
+
+    private IEnumerator FieldOfViewTransition(float targetFOV, int time)
+    {
+        time = Mathf.Max(1, time);
+        time /= 60;
+        float initalFov = cameraComponent.fieldOfView;
+        float t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / time;
+            cameraComponent.fieldOfView = Mathf.Lerp(initalFov, targetFOV, t);
+            yield return null;
+        }
+        cameraComponent.fieldOfView = targetFOV;
+    }
+
+}
