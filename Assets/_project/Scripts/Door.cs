649a2dfed6741fdc1cced2934a5423da7a3f9c28
Dranak <robin.lacomblez@gmail.com>
2020-02-29 18:01:11 +0100

    door

---
+++ Assets/_project/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public DoorButton DoorButton;
+    public Animator AnimationOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimationOpen.SetBool("IsOpen", DoorButton.isActived);
+
+    }
+
+}
