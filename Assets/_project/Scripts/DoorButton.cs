80d55897f6c55fafa1bfdbbc34a669923c058e99
skyfizz <gaetan.krol@gmail.com>
2020-03-01 15:15:20 +0100

    Hitler

---
+++ Assets/_project/Scripts/DoorButton.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour
+{
+    public bool isActived = false;
+    public GameObject UI_pressKey;
+    public AudioSource AudioSource;
+    public AudioClip AudioClipOpen;
+    public AudioClip AudioClipClose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI_pressKey.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        Debug.Log(other.gameObject.name.ToString());
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (!isActived)
+                {
+                    // Debug.Log("WESH");
+                    isActived = true;
+                    AudioSource.PlayOneShot(AudioClipOpen, .2f);
+
+                }
+                else if (isActived)
+                {
+                    isActived = false;
+                    AudioSource.PlayOneShot(AudioClipClose, 0.2f);
+
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (!isActived)
+            {
+                UI_pressKey.SetActive(true);
+            }
+        }
+    }
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            UI_pressKey.SetActive(false);
+        }
+
+
+    }
+}
